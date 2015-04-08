d9285f237e39c3f9e51714a1e5d898606025d631
gcollioucloren <gcollioucloren@enseirb-matmeca.fr>
2015-04-08 17:00:05 +0200

    First functions in clarkheuristic

--- TOSEND/src/ClarkeWrightHeuristic.java
+++ TOSEND/src/ClarkeWrightHeuristic.java
@@ -1,11 +1,24 @@
 import java.util.*
-
-    public class ClarkeWrightHeuristic(){
+    
+    public class ClarkeWrightHeuristic{
 	
-	public double computeSolution(VRPinstance instance, java.util.List<Tour>solution)
+	public double computeSolution(VRPinstance instance, java.util.List<Tour>solution){
+	    int length = solution.getMatrix().length;
+	    initsolution(instance, solution, length);
+	    
+	    
+	    
+	}
 	
-
-	
+	public void initSolution(VRPinstance instance , java.util.List<Tour>solution, int length){
+	    for (i = 1; i < solution.getMatrix().length ; i++){
+		Tour ttmp = Tour(instance , i);
+		solution.append(ttmp);
+	    }	    	    
+	}
 	
     }
 
+}
+
+	
